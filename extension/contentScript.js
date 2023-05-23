bd13756d0dbe4482062ee6aeafcf8f378113af86
Andrew Morris <voltrevo@gmail.com>
2023-05-23 13:56:57 +1000

    It works

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,7 +1,9 @@
 'use strict';
 
-const browser = globalThis.browser ?? globalThis.chrome;
+(() => {
+  const browser = globalThis.browser ?? globalThis.chrome;
 
-const pageScriptTag = document.createElement('script');
-pageScriptTag.src = browser.runtime.getURL('pageContentScript.js');
-document.documentElement.appendChild(pageScriptTag);
+  const pageScriptTag = document.createElement('script');
+  pageScriptTag.src = browser.runtime.getURL('pageContentScript.js');
+  document.documentElement.appendChild(pageScriptTag);
+})();
--- extension/pageContentScript.js
+++ extension/pageContentScript.js
@@ -1,1 +1,47 @@
-window.test = true;
+'use strict';
+
+(() => {
+  let lastPasteHotkeyTime = -Infinity;
+
+  document.addEventListener('keydown', (e) => {
+    if (e.metaKey && e.key === 'v') {
+      lastPasteHotkeyTime = Date.now();
+    }
+  });
+
+  function isClipboardAllowed() {
+    return Date.now() - lastPasteHotkeyTime < 500;
+  };
+
+  const clipboard = navigator.clipboard;
+
+  const originalClipboardRead = clipboard.read.bind(clipboard);
+
+  navigator.clipboard.read = async (...args) => {
+    // Call it for perms regardless
+    const result = await originalClipboardRead(...args);
+
+    if (isClipboardAllowed()) {
+      return result;
+    }
+
+    console.warn('Clipboard Guard: blocked clipboard.read');
+
+    return [];
+  };
+
+  const originalClipboardReadText = clipboard.readText.bind(clipboard);
+
+  navigator.clipboard.readText = async (...args) => {
+    // Call it for perms regardless
+    const result = await originalClipboardReadText(...args);
+
+    if (isClipboardAllowed()) {
+      return result;
+    }
+
+    console.warn('Clipboard Guard: blocked clipboard.readText');
+
+    return '';
+  };
+})();
