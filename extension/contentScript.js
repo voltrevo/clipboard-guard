cace3842e303653c8caabdad44df1609064d6fd5
Andrew Morris <voltrevo@gmail.com>
2023-05-23 12:30:22 +1000

    Fixes

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const browser = globalThis.browser ?? globalThis.chrome;
+
 const pageScriptTag = document.createElement('script');
 pageScriptTag.src = browser.runtime.getURL('pageContentScript.js');
 document.documentElement.appendChild(pageScriptTag);
